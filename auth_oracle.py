13a9763103ed9396f1218014ec299a971735ae97
Sean Berry <95090904+spberry4@users.noreply.github.com>
2023-09-19 16:17:37 -0600

    Update auth_oracle.py
    updated sqlalchemy string with variables

--- auth_oracle.py
+++ auth_oracle.py
@@ -1,14 +1,14 @@
 import sqlalchemy
 from sqlalchemy.engine import create_engine
 import pandas as pd
-from oracle_query import query_oracle
+from oracle_query import query_oracle, password, username, service_name, server_name
 import cx_Oracle
 import numpy as np
 
 
 #authenticate oracle and returns a dataframe
 def auth_oracle():
-    engine = create_engine("sqlalchemy string")
+    engine = create_engine(f"oracle+cx_oracle://{username}:{password}@{server_name}/?service_name={service_name}")
 
     metadata = sqlalchemy.MetaData(engine)
     print(metadata)
@@ -16,3 +16,6 @@ def auth_oracle():
     df_oracle = pd.read_sql(query_oracle, con = engine)
 
     return df_oracle
+
+#df_oracle = auth_oracle()
+#print(df_oracle)
